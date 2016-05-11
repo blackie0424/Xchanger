468c05b996d66bb5b2738a33929e412db64e2024
blackie0424 <blackie0424@gmail.com>
2016-05-11 15:45:06 +0800

    show 10 hostels to maps

--- api/controllers/CityController.js
+++ api/controllers/CityController.js
@@ -7,11 +7,15 @@
 
 module.exports = {
 	index: function (req, res) {
-		return res.view("city/index");
+		Hostel.find().limit(10).exec(function(err,hostels){
+			console.log(hostels);
+			return res.view("city/index",{hostels:hostels});
+		});
+		
   },
   show: function(req, res){
-  		var city = req.param('city');
-	  	Hostel.find({city:city,type:["民宿","好客民宿"]}).exec(function(err,hostels){
+		var city = req.param('city');
+    Hostel.find({city:city,type:["民宿","好客民宿"]}).exec(function(err,hostels){
 			return res.view("city/show",{hostels:hostels,city:city});
 		});
   }
