429fea239794473a6c95b91b9701df66e7f3077c
blackie0424 <blackie0424@gmail.com>
2016-05-23 17:27:11 +0800

    updated_at filed will auto update time

--- db/migrations/20160521034337_hostel_table.js
+++ db/migrations/20160521034337_hostel_table.js
@@ -3,14 +3,14 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('hostels', function(table) {
   		table.increments('id');
-  		table.string('name');
-  		table.string('address');
+  		table.string('name').notNullable() ;
+  		table.string('address').notNullable();
   		table.string('phone');
   		table.string('email');
   		table.string('website');
   		table.string('polis');
-  		table.timestamps();
-	})
+      table.timestamp('updated_at');
+    })
 };
 
 exports.down = function(knex, Promise) {
